4b5dac5937448f55cf2ef86c8f9bf361880d31c4
Joseph <underradicals@gmail.com>
2025-09-30 07:03:52 -0500

    Add Services: - Localization -- list of supported languages - ManifestService -- class to handle manifest data
    On branch manifest
    Your branch is ahead of 'origin/manifest' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   services/__init__.py
    	new file:   services/localization.py
    	new file:   services/manifest_service.py
    
    Changes not staged for commit:
    	modified:   .gitignore
    	modified:   main.py
    	modified:   requirements.txt
    
    Untracked files:
    	config/

---
+++ services/__init__.py
@@ -0,0 +1,8 @@
+from services.manifest_service import ManifestService
+from services.localization import Localization
+
+
+__all__ = ["ManifestService", "Localization"]
+
+if __name__ == "__main__":
+    pass
